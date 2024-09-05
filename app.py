6220e5e7a260b280158f952bc99e0b36536a95bc
Hammad Majeed <hammadmajeed@gmail.com>
2024-09-05 12:25:57 +0500

    made some changes for the deplyment

--- app.py
+++ app.py
@@ -17,5 +17,11 @@ def add(x, y):
     print("I am inside add")
     return str(int(x) + int(y))
 
+@app.route('/change/sub/<x>/<y>', methods=['GET'])
+def sub(x, y):
+    print("I am inside subtract")
+    return str(int(x) - int(y))
+
+
 if __name__ == '__main__':
     app.run( host='0.0.0.0', port=5000, debug=True) # note that host='0.0.0.0' is imp for external access
